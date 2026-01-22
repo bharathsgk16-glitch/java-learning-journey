abb0ece6b062400ce6cd13cda7d065738ffe396c
bharathsgk16-glitch <bharath.sgk.16@gmail.com>
2026-01-22 09:39:00 +0530

    Practices of Variables and DataTypes

---
+++ src/basic_fundamentals/VariableOpdemo.java
@@ -0,0 +1,51 @@
+package basic_fundamentals;
+
+public class VariableOpdemo {
+    public static void main(String[] args) {
+
+    String name = "Bharath";
+    int age =18;
+    String city ="Coimbatore";
+    boolean isEmoloyed =true;
+        System.out.println("My name is"+ name);
+        System.out.println("My age is"+ age);
+        System.out.println("My city is"+ city);
+        System.out.println("I employed"+isEmoloyed);
+    }
+}
+//Task-2
+class Operations{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("Sum"+(a+b));
+        System.out.println("Subract"+(a-b));
+        System.out.println("Product"+(a*b));
+        System.out.println("Divison"+(a/b));
+
+    }
+}
+
+//Tasks -3
+class primivedate{
+
+public static void main(String[] args) {
+    byte a=9;
+    short b=133;
+    int c=123;
+    long num=123455678785l;
+    boolean istrue=false;
+    float isdecimal=123.1f;
+    double salary=1234.345;
+    char inital='S';
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(num);
+    System.out.println(isdecimal);
+    System.out.println(salary);
+    System.out.println(istrue);
+    System.out.println(inital);
+}
+        }
